4c8f7b8c4241a5f9bb3c80c4058326f75661ac56
RoberthLoorDev <dayvidlife@gmail.com>
2024-11-29 11:32:12 -0500

    add: config base [lint, prettier, natiwind]

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+     content: [
+          "./App.js",
+          "./components/**/*.{js,jsx,ts,tsx}",
+          "./app/**/*.{js,jsx,ts,tsx}",
+     ],
+     theme: {
+          extend: {},
+     },
+     plugins: [],
+};
